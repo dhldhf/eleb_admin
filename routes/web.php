c6a363dac0842d322ed8b44d91c33c974d174e32
blog <1839652639@qq.com>
2018-04-20 02:00:13 +0800

    第一次提交,希望别出错

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('categories', 'CategoryController');
+
+Route::resource('businesses', 'BusinessController');
