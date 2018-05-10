4a8296178f926540237b67a87cda7f39d7746b7a
blog <1839652639@qq.com>
2018-05-11 01:17:36 +0800

    中文分词搜索

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,32 @@ Route::get('events/{event}/lottery','EventsController@lottery')->name('lottery')
 
 Route::get('events/{event}/winning','EventsController@winning')->name('winning');
 
-
+//测试中文分词搜索
+//Route::get('/search',function (){
+////    require ( "sphinxapi.php" );
+//    $cl = new \App\SphinxClient();
+//    $cl->SetServer ( '127.0.0.1', 9312);
+////$cl->SetServer ( '10.6.0.6', 9312);
+////$cl->SetServer ( '10.6.0.22', 9312);
+////$cl->SetServer ( '10.8.8.2', 9312);
+//    $cl->SetConnectTimeout ( 10 );
+//    $cl->SetArrayResult ( true );
+//// $cl->SetMatchMode ( SPH_MATCH_ANY);
+//    $cl->SetMatchMode ( SPH_MATCH_EXTENDED2);
+//    $cl->SetLimits(0, 1000);//返回多少条数据
+//    $info = '胖哥烧烤';
+//    $res = $cl->Query($info, 'information');//shopstore_search
+////print_r($cl);
+////    print_r($res);
+//    if ($res['total']){
+//        //获取商家id
+//       $datas =  collect($res['matches'])->pluck('id');
+//       dd($datas);
+//    }else{
+//
+//    }
+//
+//});
 //Route::get('/mail',function(){
 //    \Illuminate\Support\Facades\Mail::send(
 //        'mail',//邮件视图模板
